ad10e5a3c9e5c50367e71f0ea67e3be718acae32
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2023-03-07 18:36:32 +0100

    new color scheme and some animations

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -8,6 +8,8 @@ const Intro = () => {
       <div className={styles.sectionTitleMobile}>
         {t("hello").toUpperCase()}
       </div>
+      <div className={styles.circle}></div>
+      <div className={styles.secondCircle}></div>
       <h3 className={styles.secondaryTitle}>{t("i am Sonia")}</h3>
       <h3 className={styles.secondaryTitle}>{t("front-end developper")}</h3>
     </div>
